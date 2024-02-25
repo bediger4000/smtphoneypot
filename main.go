510b0c637f09c1bb9048da7dc2d1cd76d761939c
Bruce Ediger <bediger8@gmail.com>
2024-02-24 17:06:00 -0700

    SMTP honeypot main function

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"smtphoneypot/srvr"
+)
+
+func main() {
+	hn, _ := os.Hostname()
+	hostName := flag.String("hn", hn, "host name to present to senders")
+	address := flag.String("a", "localhost:8976", "hostname:port form address on which to listen")
+	logDirectory := flag.String("l", "./log", "log directory")
+	debug := flag.Bool("d", false, "debug output")
+
+	flag.Parse()
+
+	server, err := srvr.NewServer(*address, *hostName, *logDirectory, *debug)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := server.NextConnection()
+		if err != nil {
+			server.Debugf("making a connection: %v", err)
+		}
+		conn.Close()
+	}
+}
